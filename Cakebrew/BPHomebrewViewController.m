907774103147cc276562932b1db6b4e3b6b43a9c
Bruno Philipe <brunoph.dev@gmail.com>
2014-04-30 10:34:15 -0300

    Reloads formulas list after a successful operation.

--- Cakebrew/BPHomebrewViewController.m
+++ Cakebrew/BPHomebrewViewController.m
@@ -189,6 +189,7 @@
 - (void)homebrewManagerFinishedUpdating:(BPHomebrewManager *)manager
 {
 	[self buildSidebarTree];
+	[self.tableView_formulas reloadData];
 
 	// Used after unlocking the app when inserting custom homebrew installation path
 	BOOL shouldReselectFirstRow = ([_outlineView_sidebar selectedRow] < 0);
