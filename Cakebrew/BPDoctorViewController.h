6df491c3beb9801a09c8f43f1297d0aaa6b8b94e
Marek Hrusovsky <xhruso00@gmail.com>
2014-09-04 12:51:29 +0200

    Added separated view Controllers for update and doctor

---
+++ Cakebrew/BPDoctorViewController.h
@@ -0,0 +1,15 @@
+//
+//  BPDoctorViewController.h
+//  Cakebrew
+//
+//  Created by Marek Hrusovsky on 24/08/14.
+//  Copyright (c) 2014 Bruno Philipe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BPDoctorViewController : NSViewController
+
+- (IBAction)runStopDoctor:(id)sender;
+
+@end
