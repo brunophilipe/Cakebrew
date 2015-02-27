4089ede97f267174e93f4e372a9dc440634f5d7b
Bruno Philipe <brunophilipe@me.com>
2015-02-27 02:49:14 -0300

    Added check for preferredMaxLayoutWidth support, which is not available in all supported versions.

--- Cakebrew/BPSelectedFormulaViewController.m
+++ Cakebrew/BPSelectedFormulaViewController.m
@@ -17,86 +17,89 @@
 
 - (void)awakeFromNib
 {
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(updatePreferedWidth:)
-                                               name:NSViewFrameDidChangeNotification
-                                             object:self.view];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+											 selector:@selector(updatePreferedWidth:)
+												 name:NSViewFrameDidChangeNotification
+											   object:self.view];
 }
 
 - (void)dealloc
 {
-  [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                  name:NSViewFrameDidChangeNotification
-                                                object:self.view];
+	[[NSNotificationCenter defaultCenter] removeObserver:self
+													name:NSViewFrameDidChangeNotification
+												  object:self.view];
 }
 
 - (void)updatePreferedWidth:(id)sender
 {
-  self.formulaDependenciesLabel.preferredMaxLayoutWidth = self.formulaDependenciesLabel.frame.size.width;
-  self.formulaConflictsLabel.preferredMaxLayoutWidth = self.formulaConflictsLabel.frame.size.width;
-  self.formulaVersionLabel.preferredMaxLayoutWidth = self.formulaVersionLabel.frame.size.width;
-  self.formulaPathLabel.preferredMaxLayoutWidth = self.formulaPathLabel.frame.size.width;
-  [[self view] layout];
+	if ([self.formulaDependenciesLabel respondsToSelector:@selector(preferredMaxLayoutWidth)])
+	{
+		self.formulaDependenciesLabel.preferredMaxLayoutWidth = self.formulaDependenciesLabel.frame.size.width;
+		self.formulaConflictsLabel.preferredMaxLayoutWidth = self.formulaConflictsLabel.frame.size.width;
+		self.formulaVersionLabel.preferredMaxLayoutWidth = self.formulaVersionLabel.frame.size.width;
+		self.formulaPathLabel.preferredMaxLayoutWidth = self.formulaPathLabel.frame.size.width;
+		[[self view] layout];
+	}
 }
 
 - (NSString *)nibName
 {
-  return @"BPSelectedFormula";
+	return @"BPSelectedFormula";
 }
 
 - (void)setFormulae:(NSArray *)formulae
 {
-  _formulae = formulae;
-  [self displayInformationForFormulae];
+	_formulae = formulae;
+	[self displayInformationForFormulae];
 }
 
 - (void)displayInformationForFormulae
 {
 	static NSString *emptyString = @"--";
-  static NSString *multipleString = @"Multiple values";
-  
-  if (!self.formulae || [self.formulae count] == 0) {
-    [self.formulaPathLabel setStringValue:emptyString];
-    [self.formulaVersionLabel setStringValue:emptyString];
-    [self.formulaDependenciesLabel setStringValue:emptyString];
-    [self.formulaConflictsLabel setStringValue:emptyString];
-  }
-  if ([self.formulae count] == 1) {
-    BPFormula *formula = [self.formulae firstObject];
-    [formula getInformation];
-    if (formula.isInstalled) {
-      if ([formula.installPath length]) {
-        [self.formulaPathLabel setStringValue:formula.installPath];
-      } else {
-        [self.formulaPathLabel setStringValue:emptyString];
-      }
-    } else {
-      [self.formulaPathLabel setStringValue:@"Formula Not Installed."];
-    }
-    if (formula.latestVersion) {
-      [self.formulaVersionLabel setStringValue:formula.latestVersion];
-    } else {
-      [self.formulaVersionLabel setStringValue:emptyString];
-    }
-    
-    if (formula.dependencies) {
-      [self.formulaDependenciesLabel setStringValue:formula.dependencies];
-    } else {
-      [self.formulaDependenciesLabel setStringValue:@"This formula has no dependencies!"];
-    }
-    
-    if (formula.conflicts) {
-      [self.formulaConflictsLabel setStringValue:formula.conflicts];
-    } else {
-      [self.formulaConflictsLabel setStringValue:@"This formula has no known conflicts."];
-    }
-  }
-  if ([self.formulae count] > 1) {
-    [self.formulaPathLabel setStringValue:multipleString];
-    [self.formulaDependenciesLabel setStringValue:multipleString];
-    [self.formulaConflictsLabel setStringValue:multipleString];
-    [self.formulaVersionLabel setStringValue:multipleString];
-  }
+	static NSString *multipleString = @"Multiple values";
+	
+	if (!self.formulae || [self.formulae count] == 0) {
+		[self.formulaPathLabel setStringValue:emptyString];
+		[self.formulaVersionLabel setStringValue:emptyString];
+		[self.formulaDependenciesLabel setStringValue:emptyString];
+		[self.formulaConflictsLabel setStringValue:emptyString];
+	}
+	if ([self.formulae count] == 1) {
+		BPFormula *formula = [self.formulae firstObject];
+		[formula getInformation];
+		if (formula.isInstalled) {
+			if ([formula.installPath length]) {
+				[self.formulaPathLabel setStringValue:formula.installPath];
+			} else {
+				[self.formulaPathLabel setStringValue:emptyString];
+			}
+		} else {
+			[self.formulaPathLabel setStringValue:@"Formula Not Installed."];
+		}
+		if (formula.latestVersion) {
+			[self.formulaVersionLabel setStringValue:formula.latestVersion];
+		} else {
+			[self.formulaVersionLabel setStringValue:emptyString];
+		}
+		
+		if (formula.dependencies) {
+			[self.formulaDependenciesLabel setStringValue:formula.dependencies];
+		} else {
+			[self.formulaDependenciesLabel setStringValue:@"This formula has no dependencies!"];
+		}
+		
+		if (formula.conflicts) {
+			[self.formulaConflictsLabel setStringValue:formula.conflicts];
+		} else {
+			[self.formulaConflictsLabel setStringValue:@"This formula has no known conflicts."];
+		}
+	}
+	if ([self.formulae count] > 1) {
+		[self.formulaPathLabel setStringValue:multipleString];
+		[self.formulaDependenciesLabel setStringValue:multipleString];
+		[self.formulaConflictsLabel setStringValue:multipleString];
+		[self.formulaVersionLabel setStringValue:multipleString];
+	}
 }
 
 @end
