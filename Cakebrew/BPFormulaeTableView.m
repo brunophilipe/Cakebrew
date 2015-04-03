05286772fce31be824f1029b1b572a55da983d7c
Bruno Philipe <brunophilipe@me.com>
2015-04-03 15:21:41 -0300

    Corrected indentation.

--- Cakebrew/BPFormulaeTableView.m
+++ Cakebrew/BPFormulaeTableView.m
@@ -18,29 +18,29 @@ NSString * const kColumnIdentifierName = @"Name";
 
 - (void)awakeFromNib
 {
-  [self addObserver:self
-         forKeyPath:NSStringFromSelector(@selector(mode))
-            options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew
-            context:BPFormulaeTableViewContext];
+	[self addObserver:self
+		   forKeyPath:NSStringFromSelector(@selector(mode))
+			  options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew
+			  context:BPFormulaeTableViewContext];
 }
 
 - (instancetype)initWithFrame:(NSRect)frameRect
 {
-  self = [super initWithFrame:frameRect];
-  if (self) {
-    _mode = kBPListAll;
-  }
-  return self;
+	self = [super initWithFrame:frameRect];
+	if (self) {
+		_mode = kBPListAll;
+	}
+	return self;
 }
 
 - (void)configureTableForListing
 {
 	CGFloat totalWidth = 0;
 	NSInteger titleWidth = 0;
-  
-  //OUR superview is NSClipView
-  totalWidth = [[self superview] frame].size.width;
-  
+	
+	//OUR superview is NSClipView
+	totalWidth = [[self superview] frame].size.width;
+	
 	switch (self.mode) {
 		case kBPListAll:
 			titleWidth = (NSInteger)(totalWidth - 125);
@@ -50,7 +50,7 @@ NSString * const kColumnIdentifierName = @"Name";
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setWidth:(totalWidth-titleWidth)*0.90];
 			[self setAllowsMultipleSelection:NO];
 			break;
-      
+			
 		case kBPListInstalled:
 			titleWidth = (NSInteger)(totalWidth * 0.4);
 			[[self tableColumnWithIdentifier:kColumnIdentifierVersion] setHidden:NO];
@@ -59,7 +59,7 @@ NSString * const kColumnIdentifierName = @"Name";
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setHidden:YES];
 			[self setAllowsMultipleSelection:NO];
 			break;
-      
+			
 		case kBPListLeaves:
 			titleWidth = (NSInteger)(totalWidth * 0.99);
 			[[self tableColumnWithIdentifier:kColumnIdentifierVersion] setHidden:YES];
@@ -67,7 +67,7 @@ NSString * const kColumnIdentifierName = @"Name";
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setHidden:YES];
 			[self setAllowsMultipleSelection:NO];
 			break;
-      
+			
 		case kBPListOutdated:
 			titleWidth = (NSInteger)(totalWidth * 0.4);
 			[[self tableColumnWithIdentifier:kColumnIdentifierVersion] setHidden:NO];
@@ -77,7 +77,7 @@ NSString * const kColumnIdentifierName = @"Name";
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setHidden:YES];
 			[self setAllowsMultipleSelection:YES];
 			break;
-      
+			
 		case kBPListSearch:
 			titleWidth = (NSInteger)(totalWidth - 90);
 			[[self tableColumnWithIdentifier:kColumnIdentifierVersion] setHidden:YES];
@@ -86,44 +86,45 @@ NSString * const kColumnIdentifierName = @"Name";
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setWidth:(totalWidth-titleWidth)*0.90];
 			[self setAllowsMultipleSelection:NO];
 			break;
-      
-    case kBPListRepositories:
+			
+		case kBPListRepositories:
 			titleWidth = (NSInteger)(totalWidth * 0.99);
 			[[self tableColumnWithIdentifier:kColumnIdentifierVersion] setHidden:YES];
 			[[self tableColumnWithIdentifier:kColumnIdentifierLatestVersion] setHidden:YES];
 			[[self tableColumnWithIdentifier:kColumnIdentifierStatus] setHidden:YES];
 			[self setAllowsMultipleSelection:NO];
-      
+			break;
+			
 		default:
 			break;
 	}
-  
+	
 	[[self tableColumnWithIdentifier:kColumnIdentifierName] setWidth:titleWidth];
-  [self setNeedsDisplay:YES];
+	[self setNeedsDisplay:YES];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-  if (context == BPFormulaeTableViewContext) {
-    if ([object isEqualTo:self]) {
-      if([keyPath isEqualToString:NSStringFromSelector(@selector(mode))]){
-        [self configureTableForListing];
-      }
-    }
-  } else {
-    @try {
-      [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
-    }
-    @catch (NSException *exception) {}
-    @finally {}
-  }
+	if (context == BPFormulaeTableViewContext) {
+		if ([object isEqualTo:self]) {
+			if([keyPath isEqualToString:NSStringFromSelector(@selector(mode))]){
+				[self configureTableForListing];
+			}
+		}
+	} else {
+		@try {
+			[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+		}
+		@catch (NSException *exception) {}
+		@finally {}
+	}
 }
 
 - (void)dealloc
 {
-  [self removeObserver:self
-            forKeyPath:NSStringFromSelector(@selector(mode))
-               context:BPFormulaeTableViewContext];
+	[self removeObserver:self
+			  forKeyPath:NSStringFromSelector(@selector(mode))
+				 context:BPFormulaeTableViewContext];
 }
 
 @end
